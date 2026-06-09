936011ebe9c4d40e4033569ef7af91ec1fbf7df9
khubaib <muhammadkhubaib279@gmail.com>
2026-06-10 02:44:57 +0500

    logo and icon changed

--- vite.config.ts
+++ vite.config.ts
@@ -31,13 +31,13 @@ export default defineConfig({
         categories: ["business", "productivity"],
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/KhataX%20192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "/icon-512.png",
+            src: "/KhataX%20512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
